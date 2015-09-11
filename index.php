df98ca95a75f02a5d09b7c99826eb62311fe1bca
TheLawrence <lawsarmiento@gmail.com>
2015-09-11 12:04:45 -0700

    Commented the homepage

--- index.php
+++ index.php
@@ -15,8 +15,10 @@ and open the template in the editor.
         //    echo 'Super!';
         include('student.php');
         
+            //Creating an array called students
             $students = array();
             
+            //Creating the 1st student and adding it to the students array
             $first = new Student();
             $first->surname = "Doe";
             $first->first_name = "John";
@@ -27,6 +29,7 @@ and open the template in the editor.
             $first->add_grade(55);
             $students['j123'] = $first;
             
+            //Creating 2nd student and adding it to the students array
             $second = new Student();
             $second->surname = "Einstein";
             $second->first_name = "Albert";
@@ -38,6 +41,7 @@ and open the template in the editor.
             $second->add_grade(50);
             $students['a456'] = $second;
             
+            //Creating 3rd student and adding it to the students array
             $third = new Student();
             $third->surname = "Sarmiento";
             $third->first_name = "Lawrence";
@@ -47,8 +51,10 @@ and open the template in the editor.
             $third->add_grade(1);
             $students['a123'] = $third;
             
+            //sort the students array before displaying the students
             ksort($students);
             
+            //Display each student in the students array
             foreach($students as $student)
                 echo $student->toString();
         ?>
